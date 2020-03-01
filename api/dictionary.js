de6485baa4bedbb7bb73e3d8abc7a0783d5d23cb
Shivam Shekhar <shivamshekhar@197NODMB5002168.local>
2020-03-01 22:26:37 +0530

    added dict defn function

--- api/dictionary.js
+++ api/dictionary.js
@@ -15,60 +15,51 @@ const relatedWordsClientObject = new RelatedWordsClient();
 class Dictionary {
     static async getWordDefinition(word) {
         try {
-            //TODO : add string type checks
+            if(typeof(word) !== 'string') {
+                throw new Error(`Provided word ${word} is not a valid string`);
+            }
+
             let response = await wordDefinitionClientObject.with(word);
             return response;
         } catch(err) {
-            // TODO : log
             throw err;
         }
     }
 
     static async getWordExamples(word) {
         try {
-            //TODO : add string type checks
+            if(typeof(word) !== 'string') {
+                throw new Error(`Provided word ${word} is not a valid string`);
+            }
+
             let response = await wordExamplesClientObject.with(word);
             return response;
         } catch(err) {
-            // TODO : log
             throw err;
         }
     }
 
     static async getRandomWord() {
         try {
-            //TODO : add string type checks
             let response = await randomWordClientObject.with();
             return response;
         } catch(err) {
-            // TODO : log
             throw err;
         }
     }
 
     static async getRelatedWords(word) {
         try {
-            //TODO : add string type checks
+            if(typeof(word) !== 'string') {
+                throw new Error(`Provided word ${word} is not a valid string`);
+            }
+
             let response = await relatedWordsClientObject.with(word);
             return response;
         } catch(err) {
-            // TODO : log
             throw err;
         }
     }
 }
 
 module.exports = Dictionary;
-
-// standalone runner function for function testing and debugging
-(async function() {
-    if(require.main == module) {
-        try {
-            console.log(await Dictionary.getRelatedWords("house"));
-            process.exit(0);
-        } catch(err) {
-            console.log('Some error occurred', err);
-            process.exit(1);
-        }
-    }
-}())
