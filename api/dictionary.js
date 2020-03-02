c6d8bd08a46ab8e44e388ec9f38c3dfa0d34f424
Shivam Shekhar <shivamshekhar@197NODMB5002168.local>
2020-03-02 23:58:57 +0530

    made the gameplay function event based

--- api/dictionary.js
+++ api/dictionary.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const readLine = require('readline');
+const readline = require('readline');
+const EventEmitter = require('events');
 const L = require('../lib').logger;
 const LibUtils = require('../lib').utils;
 const constants = require('../config').constants;
@@ -17,6 +18,137 @@ const wordExamplesClientObject = new WordExamplesClient();
 const relatedWordsClientObject = new RelatedWordsClient();
 
 const logtag = '[api/dictionary]';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const rlQuestionPromisified = (question) => {
+    return new Promise(resolve => {
+        rl.question(question, resolve);
+    });
+};
+
+const getRandomDefinition = (wordDefinitions) => {
+    const randomIndex = LibUtils.getRandomIntInRange(0, (wordDefinitions.length - 1));
+    return wordDefinitions[randomIndex].text;
+};
+
+const getRandomRelatedWord = (words = []) => {
+    return words[LibUtils.getRandomIntInRange(0, words.length - 1)];
+};
+
+class Game {
+    constructor(...args) {
+        this.gameState = new GameState();
+    }
+
+    gameplay() {
+        let promise = new Promise((resolve, reject) => {
+            this.gameState
+            .on('start', async () => {
+                await this._start();
+            })
+            .on('create', async () => {
+                await this._create();
+            })
+            .on('play', async () => {
+                await this._play();
+            })
+            .on('choice', async () => {
+                await this._choice();
+            })
+            .on('hint', async () => {
+                await this._hint();
+            })
+            .on('exit', resolve)
+            .on('error', err => {
+                this.gameState.close();
+                return reject(err);
+            });
+        });
+
+        this.gameState.emit('start');
+        return promise;
+    }
+
+    async _start() {
+        L.info(`---------------- LET'S PLAY : GUESS THE WORD! ----------------\n`);
+        await rlQuestionPromisified(`\nInstructions :\nYou would be provided with a definition, a synonym or an antonym and you need to guess what the original word is!\n\nPress Enter to begin!`);
+        return this.gameState.emit('create');
+    }
+
+    async _create() {
+        this.randomWord = (await Dictionary.getRandomWord()).word;
+
+        let wordDefinitions = await Dictionary.getWordDefinition(this.randomWord);
+        this.wordDefinition = getRandomDefinition(wordDefinitions);
+        
+        let relatedWords = await Dictionary.getRelatedWords(this.randomWord);
+
+        let antonyms = Dictionary._parseRelatedWordsResponse(relatedWords, constants.WORD_RELATIONSHIP_TYPES.ANTONYM).words;
+        this.antonym = getRandomRelatedWord(antonyms);
+
+        let synonyms = Dictionary._parseRelatedWordsResponse(relatedWords, constants.WORD_RELATIONSHIP_TYPES.SYNONYM).words;
+        this.synonym = getRandomRelatedWord(synonyms);
+
+        return this.gameState.emit('play');
+    }
+
+    async _play() {
+        L.info(`-----------------------------------------------------------------\n`);
+        L.info(`Definition : ${this.wordDefinition}\n`);
+
+        if (this.synonym) {
+            L.info(`Synonym : ${this.synonym}\n`);
+        }
+
+        if (this.antonym) {
+            L.info(`Antonym : ${this.antonym}\n`);
+        }
+
+        const answer = (await rlQuestionPromisified(`Guess the word : `)) || '';
+
+        if (answer.toString().toLowerCase() === this.randomWord) {
+            L.info(`\nYou guessed it correctly! The word is ${this.randomWord}`);
+            return this.gameState.emit('exit');
+        } else {
+            L.info(`\nProvided answer ${answer} is incorrect!\n\n`);
+            return this.gameState.emit('choice');
+        }
+    }
+
+    async _choice() {
+        L.info(`-----------------------------------------------------------------\n`);
+        L.info(`1 : Try Again\n\n2 : Get a hint\n\n3 : Quit\n\n`);
+        const choice = await rlQuestionPromisified(`Select an option to proceed : `);
+
+        switch(choice) {
+            case '1' :
+                return this.gameState.emit('play');
+            case '2' :
+                return this.gameState.emit('hint');
+            case '3' :
+                return this.gameState.emit('exit');
+            default:
+                L.info(`Incorrect choice : ${choice}. Please try again!`);
+                return this.gameState.emit('choice');
+        }
+    }
+
+    async _hint() {
+        L.info(`-----------------------------------------------------------------\n`);
+        L.info(`Hint : ${this.randomWord}`);
+        return this.gameState.emit('play');
+    }
+}
+
+class GameState extends EventEmitter {
+    constructor(...args) {
+        super(...args);
+    }
+}
 
 class Dictionary {
     static async getWordDefinition(word) {
@@ -72,7 +204,7 @@ class Dictionary {
         let finalResponse = {};
 
         if(!Object.values(constants.WORD_RELATIONSHIP_TYPES).includes(relationshipType)) {
-            throw new Error(`Unsupported relationship type ${relationshipType} provided for getting related words for ${word}`);
+            throw new Error(`Unsupported relationship type ${relationshipType} provided for getting related words`);
         }
 
         if(relationshipType === constants.WORD_RELATIONSHIP_TYPES.ALL) {
@@ -86,7 +218,7 @@ class Dictionary {
             }
         }
 
-        return finalResponse
+        return finalResponse;
     }
 
     static async getAll(word) {
@@ -108,60 +240,13 @@ class Dictionary {
         }
     }
 
-    static gameplay() {
-        const rl = readLine.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-
-        const rlQuestionPromisified = (question) => {
-            return new Promise(resolve => {
-                rl.question(question, resolve);
-            });
-        };
-
-
-
-        const getRandomDefinition = (wordDefinitions) => {
-            const randomIndex = LibUtils.getRandomIntInRange(0, (wordDefinitions.length - 1));
-            return wordDefinitions[randomIndex].text;
-        };
-
-        return new Promise(async (resolve, reject) => {
-            L.info(`---------------- LET'S PLAY : GUESS THE WORD! ----------------`);
-            await rlQuestionPromisified(`\nInstructions :\nYou would be provided with a definition, a synonym or an antonym and you need to guess what the original word is!\n\nPress Enter to begin!`);
-
-            while (true) {
-                try {
-                    const randomWord = (await this.getRandomWord()).word;
-                    const relatedWords = await this.getRelatedWords(randomWord);
-                    const wordDefinitions = await this.getWordDefinition(randomWord);
-
-                    let antonyms = this._parseRelatedWordsResponse(relatedWords, constants.WORD_RELATIONSHIP_TYPES.ANTONYM).words; 
-                    let synonyms = this._parseRelatedWordsResponse(relatedWords, constants.WORD_RELATIONSHIP_TYPES.SYNONYM).words;
-
-                    L.info(`-----------------------------------------------------------------`);
-                    L.info(`Definition : ${getRandomDefinition(wordDefinitions)}\n`);
-
-                    if (synonyms) {
-                        L.info(`Synonym : ${synonyms[LibUtils.getRandomIntInRange(0, synonyms.length - 1)]}\n`);
-                    }
-
-                    if (antonyms) {
-                        L.info(`Antonym : ${antonyms[LibUtils.getRandomIntInRange(0, antonyms.length - 1)]}\n`);
-                    }
-
-                    const answer = await rlQuestionPromisified(`Guess the word : `);
-                    console.log(answer, (answer === randomWord), randomWord);
-                    break;
-                } catch (err) {
-                    console.log(err);
-                    return reject(err);
-                }
-            }
-
-            return resolve();
-        });
+    static async gameplay() {
+        try {
+            let game = new Game();
+            await game.gameplay();
+        } catch(err) {
+            throw err;
+        }
     }
 }
 
